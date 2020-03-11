0bed07b4d3ac0db3268d52232317e75e0819e244
Oscar <osaharoy@gmail.com>
2020-03-11 16:05:22 +0000

    appear

---
+++ js/appear.js
@@ -0,0 +1,29 @@
+// Heron Creative 2020
+
+function appear() {
+
+	var appear_divs = Array.from(document.querySelectorAll(".appear"));
+
+	function updateAppear() {
+
+		for(var i=0; i<appear_divs.length; ++i) {
+
+			var div        = appear_divs[i];
+			var rect       = div.getBoundingClientRect();
+			var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+			var onscreen   = rect.top - viewHeight <= 0;
+
+			if(onscreen) {
+				div.classList.remove("appear");
+				div.classList.add("slide-up-fade");
+
+				appear_divs.splice(i, 1);
+			}
+		}
+	}
+
+	document.addEventListener("scroll", updateAppear);
+	updateAppear();
+}
+
+document.addEventListener("DOMContentLoaded", appear);
